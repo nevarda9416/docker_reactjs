38354708ead7316f7a4480447844f37e8f8e1c7e
tudao <nevarda9416@gmail.com>
2025-02-15 19:11:46 +0700

    - [ReactJS + Docker]: Select, Checks and radios (Default stacked)

--- react-app/src/components/AdvancedBootstrapForm.tsx
+++ react-app/src/components/AdvancedBootstrapForm.tsx
@@ -19,6 +19,43 @@ const AdvancedBootstrapForm = () => {
     }
     return (
         <Form>
+            {['checkbox', 'radio'].map((type) => (
+                <div key={`default-${type}`} className="mb-3">
+                    <Form.Check
+                        id={`default-${type}`}
+                        label={`default ${type}`}/>
+                    <Form.Check
+                        disabled
+                        id={`disabled-default-${type}`}
+                        label={`disabled ${type}`}/>
+                </div>
+            ))}
+            <Form.Select size="lg">
+                <option>Large select</option>
+            </Form.Select>
+            <br/>
+            <Form.Select>
+                <option>Default select</option>
+            </Form.Select>
+            <br/>
+            <Form.Select size="sm">
+                <option>Small select</option>
+            </Form.Select>
+            <br/>
+            <Form.Select aria-label="Default select example">
+                <option>Open this select menu</option>
+                <option value="1">One</option>
+                <option value="2">Two</option>
+                <option value="3">Three</option>
+            </Form.Select>
+            <br/>
+            <Form.Label htmlFor="inputPassword">Password</Form.Label>
+            <Form.Control type="password" id="inputPassword" aria-describedby="passwordHelpBlock"/>
+            <Form.Text id="passwordHelpBlock" muted>
+                Your password must be 8-20 characters long, contain letters and numbers,
+                and must not contain spaces, special characters, or emoji.
+            </Form.Text>
+            <br/>
             <Form.Label htmlFor="exampleColorInput">Color picker</Form.Label>
             <Form.Control type="color" id="exampleColorInput"
                 defaultValue="#563d7c" title="Choose your color"/>
