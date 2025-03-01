1848d6d102f195288b3c5df7665c5219018b5f46
tudao <nevarda9416@gmail.com>
2025-03-01 22:20:08 +0700

    - [ReactJS + Docker]: Build pages (Profile)

--- ecommerce-app/src/App.tsx
+++ ecommerce-app/src/App.tsx
@@ -1,44 +1,48 @@
-import { Provider } from 'react-redux';
+import {Provider} from 'react-redux';
 import './App.css';
-import { store } from "./redux/store";
+import {store} from "./redux/store";
 import Navbar from "./components/NavBar";
-import { Route, Routes } from "react-router-dom";
+import {Route, Routes} from "react-router-dom";
 import Home from "./pages/Home";
-import { Toaster } from "react-hot-toast";
+import {Toaster} from "react-hot-toast";
 import Footer from "./components/Footer";
 import Cart from "./components/Cart";
 import SingleProduct from "./pages/SingleProduct";
 import LoginModal from "./components/LoginModal";
 import Wishlist from "./pages/Wishlist";
+import ProtectedRoute from './components/ProtectedRoute';
+import Profile from "./pages/Profile";
 import AllProducts from "./pages/AllProducts";
 import ScrollToTopButton from './components/ScrollToTopButton';
 import BannerPopup from "./components/BannerPopup";
 import AllCategories from "./pages/AllCategories";
 import SingleCateogory from "./pages/SingleCategory";
-import ProtectedRoute from './components/ProtectedRoute';
 
 function App() {
-  return (
-    <Provider store={store}>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/products" element={<AllProducts />} />
-        <Route path="/categories" element={<AllCategories />} />
-        <Route path="/product/:productID" element={<SingleProduct />} />
-        <Route path="/category/:slug" element={<SingleCateogory />} />
-        <Route path="/wishlist" element={<ProtectedRoute />}>
-          <Route path="/wishlist" element={<Wishlist />} />
-        </Route>
-      </Routes>
-      <Toaster position="bottom-center" reverseOrder={false} />
-      <Footer />
-      <Cart />
-      <LoginModal />
-      <ScrollToTopButton />
-      <BannerPopup />
-    </Provider>
-  )
+    return (
+        <Provider store={store}>
+            <Navbar/>
+            <Routes>
+                <Route path="/" element={<Home/>}/>
+                <Route path="/products" element={<AllProducts/>}/>
+                <Route path="/categories" element={<AllCategories/>}/>
+                <Route path="/product/:productID" element={<SingleProduct/>}/>
+                <Route path="/category/:slug" element={<SingleCateogory/>}/>
+                <Route path="/wishlist" element={<ProtectedRoute/>}>
+                    <Route path="/wishlist" element={<Wishlist/>}/>
+                </Route>
+                <Route path="/account" element={<ProtectedRoute/>}>
+                    <Route path="/account" element={<Profile/>}/>
+                </Route>
+            </Routes>
+            <Toaster position="bottom-center" reverseOrder={false}/>
+            <Footer/>
+            <Cart/>
+            <LoginModal/>
+            <ScrollToTopButton/>
+            <BannerPopup/>
+        </Provider>
+    )
 }
 
 export default App
