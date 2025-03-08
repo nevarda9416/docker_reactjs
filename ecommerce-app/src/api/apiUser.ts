6a215ab4dddeeae7b5d4da8b1b5ecb5944cc3f21
tudao <daotientu@gmail.com>
2025-03-09 01:03:14 +0700

    - [ReactJS + Docker]: Api service & helper

---
+++ ecommerce-app/src/api/apiUser.ts
@@ -0,0 +1,17 @@
+import axios from "../helpers/axios";
+const fetchAllUsers = (page: number) => {
+    return axios.get(`/users?page=${page}`);
+};
+const createNewUser = (name: string, job: string) => {
+    return axios.post(`/users`, {name, job});
+};
+const updateUser = (name: string, job: string, id: number) => {
+    return axios.put(`users/${id}`, {name, job, id});
+};
+const deleteUser = (id: number) => {
+    return axios.delete(`/users/${id}`, {id});
+};
+const loginAPI = (email: string, password: string) => {
+    return axios.post(`/login`, {email, password});
+};
+export {fetchAllUsers, createNewUser, updateUser, deleteUser, loginAPI};
