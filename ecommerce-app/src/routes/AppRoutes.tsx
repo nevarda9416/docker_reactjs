0b7f9dc87ce4881c9588f6b0d2af1f842cf22dd2
tudao <daotientu@gmail.com>
2025-03-16 22:45:34 +0700

    - [ReactJS + Docker]: Lazy loading (!!! page load speed not measured !!!)

--- ecommerce-app/src/routes/AppRoutes.tsx
+++ ecommerce-app/src/routes/AppRoutes.tsx
@@ -1,39 +1,43 @@
-import {Route, Routes} from "react-router-dom";
-import Home from "../pages/Home";
-import AllProducts from "../pages/AllProducts";
-import AllCategories from "../pages/AllCategories";
-import SingleProduct from "../pages/SingleProduct";
-import SingleCategory from "../pages/SingleCategory";
-import Wishlist from "../pages/Wishlist";
-import Profile from "../pages/Profile";
-import UserList from "../pages/admin/UserList.tsx";
-import Page404 from "../pages/errors/Page404";
-import ProtectedRoute from './ProtectedRoute.tsx';
-import MultiFileUpload from "../pages/admin/MultiFileUpload.tsx";
+import React, { Suspense } from "react";
+import { Route, Routes } from "react-router-dom";
+
+const Home = React.lazy(() => import("../pages/Home"));
+const AllProducts = React.lazy(() => import("../pages/AllProducts"));
+const AllCategories = React.lazy(() => import("../pages/AllCategories"));
+const SingleProduct = React.lazy(() => import("../pages/SingleProduct"));
+const SingleCategory = React.lazy(() => import("../pages/SingleCategory"));
+const ProtectedRoute = React.lazy(() => import("./ProtectedRoute"));
+const Wishlist = React.lazy(() => import("../pages/Wishlist"));
+const Profile = React.lazy(() => import("../pages/Profile"));
+const UserList = React.lazy(() => import("../pages/admin/UserList"));
+const MultiFileUpload = React.lazy(() => import("../pages/admin/MultiFileUpload"));
+const Page404 = React.lazy(() => import("../pages/errors/Page404"));
 
 const AppRoutes = () => {
     return (
         <>
-            <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="/products" element={<AllProducts/>}/>
-                <Route path="/categories" element={<AllCategories/>}/>
-                <Route path="/product/:productID" element={<SingleProduct/>}/>
-                <Route path="/category/:slug" element={<SingleCategory/>}/>
-                <Route path="/wishlist" element={<ProtectedRoute/>}>
-                    <Route path="/wishlist" element={<Wishlist/>}/>
-                </Route>
-                <Route path="/account" element={<ProtectedRoute/>}>
-                    <Route path="/account" element={<Profile/>}/>
-                </Route>
-                <Route path="/admin/users" element={<ProtectedRoute/>}>
-                    <Route path="/admin/users" element={<UserList/>}/>
-                </Route>
-                <Route path="/admin/upload" element={<ProtectedRoute/>}>
-                    <Route path="/admin/upload" element={<MultiFileUpload/>}/>
-                </Route>
-                <Route path="*" element={<Page404/>}/>
-            </Routes>
+            <Suspense fallback={<div>Loading...</div>}>
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/products" element={<AllProducts />} />
+                    <Route path="/categories" element={<AllCategories />} />
+                    <Route path="/product/:productID" element={<SingleProduct />} />
+                    <Route path="/category/:slug" element={<SingleCategory />} />
+                    <Route path="/wishlist" element={<ProtectedRoute />}>
+                        <Route path="/wishlist" element={<Wishlist />} />
+                    </Route>
+                    <Route path="/account" element={<ProtectedRoute />}>
+                        <Route path="/account" element={<Profile />} />
+                    </Route>
+                    <Route path="/admin/users" element={<ProtectedRoute />}>
+                        <Route path="/admin/users" element={<UserList />} />
+                    </Route>
+                    <Route path="/admin/upload" element={<ProtectedRoute />}>
+                        <Route path="/admin/upload" element={<MultiFileUpload />} />
+                    </Route>
+                    <Route path="*" element={<Page404 />} />
+                </Routes>
+            </Suspense>
         </>
     );
 }
